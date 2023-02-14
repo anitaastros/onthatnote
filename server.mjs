c706795939f04698266cb4986b1e7f297797ca96
anitaastros <70915108+anitaastros@users.noreply.github.com>
2023-02-14 23:14:00 +0000

    working on server
    testing jokes in different languages

--- server.mjs
+++ server.mjs
@@ -1,11 +1,11 @@
 
-// import Joke from 'jokemaster'
 import express from 'express'
 import Joke from './node_modules/jokemaster/joke.mjs';
+import Dictionary from './node_modules/languagemodule/Dictionary.mjs';
 
-const joke = new Joke(["this is a joke", "joke no 2", "wow so many jokes"]);
-console.log(`The joke teller knows ${joke.numberOfJokes}`)
-console.log(joke.tellAJoke());
+const languages = ["./lan/no.json", "./lan/en.json"];
+const newDictionary = new Dictionary(...languages);
+
 
 const server = express();
 const port = (process.env.PORT || 8080);
@@ -13,21 +13,40 @@ const port = (process.env.PORT || 8080);
 server.set('port', port);
 server.use(express.static('public'));
 
+//need to fix how to get jokes in english 
+server.get('/en/jokes', async (req, res) => {
+  try {
+    await newDictionary.setLanguage('en');
+    console.info('Setting language as: en');
+  } catch (error) {
+    res.status(400).end();
+  }
 
-server.get('/jokes', async (req, res) => {
-  
-  
-    // Send a request to the task module to get all tasks
-    const getJokes = req.query.joke
-  
-    res.status(200).json(getJokes);
-    // return tasks;
-    
-  });
+  const englishJokes = newDictionary.get("jokes");
+  console.info('Getting english jokes: ', englishJokes);
 
+  const joke = new Joke(englishJokes);
+  console.info('Creating a new Joke instance: ', joke);
 
-// Vi starter serveren med funksjonen listen. 
-// Den krever en port så den henter vi fra det vi satt i serveren vår tidligere. 
+  const getJoke = joke.tellAJoke();
+  console.info('Joke fetched: ', getJoke);
+
+  res.status(200).json(getJoke);
+});
+
+//need to fix how to get jokes in norwegian
+server.get('/no/jokes', async (req, res) => {
+  await newDictionary.setLanguage('no');
+  console.info('Setting language as: no');
+
+  const norwegianJokes = newDictionary.get("jokes");
+
+  const joke = new Joke(norwegianJokes);
+  const getJokes = joke.tellAJoke();
+
+  res.status(200).json(getJokes);
+});
+
 server.listen(server.get('port'), function () {
-    console.log('server running', server.get('port')); 
+  console.log('server running', server.get('port'));
 });
